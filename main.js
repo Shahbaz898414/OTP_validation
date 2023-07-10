d788cbaaba6a54b567a3595d67afa1200204c8fc
shahbaz Khan <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-11 03:32:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const code=document.querySelectorAll('.code');
+
+code[0].focus();
+
+
+code.forEach((code,idx)=>{
+  code.addEventListener('keydown',(e)=>{
+    if(e.key>=0 && e.key<=9){
+      code[idx].value='';
+      setTimeout(()=> code[idx+1].focus(),10);
+    }else if(e.key==="Backspace"){
+      setTimeout(()=> code[idx-1].focus(),10);
+    }
+
+  })
+})
